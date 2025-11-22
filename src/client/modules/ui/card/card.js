683310fd407fa0f2e69c16089043e4f9d218adcd
ssk42 <sthreitz@gmail.com>
2025-11-22 13:33:42 -0500

    Add Card component to UI module for slot-based content rendering

---
+++ src/client/modules/ui/card/card.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class Card extends LightningElement {
+    // No logic needed here since we are just using <slot>, 
+    // but this file is required for the component to exist.
+}
